2bd7c34e86ee007439aafd87fd3c5115e1c26f67
diurno <diurno@equipo-00>
2025-09-23 10:32:48 +0200

    ejercicio1

---
+++ src/UNIDAD1/ACTIVIDAD3PUNTO6.java
@@ -0,0 +1,22 @@
+package UNIDAD1;
+
+import java.util.Scanner;
+
+public class ACTIVIDAD3PUNTO6 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		 System.out.print("Introduce tu nombre ");
+		 String nombre  = sc.nextLine();
+		 
+		 
+		 System.out.print("Introduce tu edad: ");
+		 int edad = sc.nextInt();
+		 
+		 
+		 System.out.println("Hola " +nombre+" , tienes "+edad+" años.");
+		 
+	}
+}
